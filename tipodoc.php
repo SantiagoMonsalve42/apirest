3dbc39389e4a2836193fe10aa93f3b32a2a85f48
santiago monsalve <santi.monsalve09@gmail.com>
2021-06-15 17:41:34 -0500

    header para peticiones con arquitectura REST y adicion de endpoint para traer tipos de documento

---
+++ tipodoc.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'config.php';
+error_reporting(0);  
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$objConfig = new configuration;
+$dbConn=$objConfig->getConection();
+
+    //read
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+
+        $sqlStatement="SELECT * FROM tipo_documento";
+        $sql = $dbConn->prepare($sqlStatement);
+        $sql->execute();
+        $sql->setFetchMode(PDO::FETCH_ASSOC);
+        header("HTTP/1.1 200 OK");
+        echo json_encode( $sql->fetchAll()  );
+
+        exit();
+
+}
+?>
